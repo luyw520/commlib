96cef142214464f078eec1ebe9728d2e7ce1cf20
luyuanwei <648422720@qq.com>
2019-01-23 15:29:39 +0800

    1 提交定位

---
+++ src/main/java/com/lu/library/annotation/DebugTrace.java
@@ -0,0 +1,13 @@
+
+package com.lu.library.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.CONSTRUCTOR, ElementType.METHOD })
+public @interface DebugTrace {}
